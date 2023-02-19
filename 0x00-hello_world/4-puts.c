5f731b3ec418dc9890fd383d6932b5753758c189
GIVEN <givensithole172@gmail.com>
2023-02-19 09:59:18 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-        return (0);
+                        return (0);
 }	
 
