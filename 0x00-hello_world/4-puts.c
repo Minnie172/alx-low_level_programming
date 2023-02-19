d15ac25c52f406097b6f8f63da02e271aef10ee7
GIVEN <givensithole172@gmail.com>
2023-02-19 10:28:52 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Return: Always 0(success)
  */
 
 int main(void)
 {
-puts("\"programmingislikebuildingamultilingualpuzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }	
