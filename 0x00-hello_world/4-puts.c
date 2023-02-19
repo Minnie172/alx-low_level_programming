5e461585f581cc35495f56a3034b54bda81d499f
GIVEN <givensithole172@gmail.com>
2023-02-19 11:17:36 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
+ * main - A program that prints a line with puts function.
  * Return: Always 0(success)
  */
-
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
